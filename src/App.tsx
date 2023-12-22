a632edf8852ddcb9a125255c5d500e4a4ede32d4
icaroxavier <icarogabriel999@icloud.com>
2023-12-22 06:30:52 -0300

    componentzation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { TodoList } from './components/TodoList'
-import { AddTodo } from './components/AddTodo'
+
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from './store'
+import { Player } from './pages/Player'
 
 function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList/>
-      <AddTodo/>
+      <Player />
     </ReduxProvider>
   )
 }
